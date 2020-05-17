eab7bb059a44d94ec50a0fcf495e2d60c2773957
Rasoul <h.rostami.r@gmail.com>
2020-05-17 12:52:11 +0430

    Grid Spacing and Card Tweaks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{asset('images/logo.png')}}" alt="logo" style="width: 30px">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,15 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex items-center mb-3">
-        <a href="{{url('/projects/create')}}">Create New Project</a>
-    </div>
-    <div class="row">
+    <header class="row py-3">
+        <h5 class="col-6 text-muted">My Projects</h5>
+        <div class="col-6 text-right">
+            <a href="{{url('/projects/create')}}" class="btn btn-primary text-dark shadow-sm border-0" style="background-color: #47CDFF">Add Project</a>
+        </div>
+    </header>
+    <main class="row">
         @forelse($projects as $project)
             <div class="'col-sm-12 col-md-4">
-                <div class="card shadow mt-3" style="height: 200px">
+                <div class="card shadow mt-4" style="height: 200px">
                     <div class="card-body">
-                        <h3 class="card-title py-2">{{ $project->title }}</h3>
+                        <h3 class="card-title py-3 pl-2 border-left border-success" style="border-width: 5px !important; border-color: #47CDFF !important; margin-left: -20px !important;">
+                            <a href="{{ url($project->path()) }}" class="text-dark card-link">
+                                {{ $project->title }}
+                            </a>
+                        </h3>
                         <p class="card-text text-muted text-justify">{{ Str::limit($project->description, 100) }}</p>
                     </div>
                 </div>
@@ -17,5 +24,5 @@
         @empty
             <div>No projects yet!</div>
         @endforelse
-    </div>
+    </main>
 @endsection
