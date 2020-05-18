5ff5aac3107367bd8e6fe3fa98c5825afe79d522
Rasoul <h.rostami.r@gmail.com>
2020-05-18 17:37:54 +0430

    Modify Create Project UI

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,19 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <form method="POST" action="{{url('/projects')}}" >
-        @csrf
-        <h1>Create a Project</h1>
-        <div class="form-group">
-            <label for="title">Title</label>
-            <input type="text"  name="title" id="title" placeholder="Title" class="form-control"/>
+    <div class="row justify-content-center">
+        <div class="card col-md-7">
+            <div class="card-body">
+                <form method="POST" action="{{url('/projects')}}" >
+                    @csrf
+                    <h1>Create a Project</h1>
+                    <div class="form-group">
+                        <label for="title" class="h5">Title</label>
+                        <input type="text"  name="title" id="title" placeholder="Title" class="form-control" required/>
+                    </div>
+                    <div class="form-group">
+                        <label for="description" class="h5">Description</label>
+                        <textarea name="description" id="description" placeholder="Description" class="form-control" required></textarea>
+                    </div>
+                    <input
+                        type="submit"
+                        value="Create Project"
+                        class="btn btn-primary text-dark shadow-sm border-0"
+                        style="background-color: #47CDFF"
+                    />
+                    <a href="{{url('/projects')}}" class='btn btn-danger'>Cancel</a>
+                </form>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="description">Description</label>
-            <textarea name="description" id="description" placeholder="Description" class="form-control"></textarea>
-        </div>
-        <input type="submit" value="Create Project" class="btn btn-primary" />
-        <a href="{{url('/projects')}}" class='btn btn-danger'>Cancel</a>
-    </form>
+    </div>
 @endsection
 
