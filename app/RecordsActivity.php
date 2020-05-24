3c417b1abb4a87a7f3a043feb2005ff01d6f989b
Rasoul <h.rostami.r@gmail.com>
2020-05-24 11:35:06 +0430

    Change activity owner from project owner to authenticated user in RecordActivity.php

--- app/RecordsActivity.php
+++ app/RecordsActivity.php
@@ -76,7 +76,7 @@
         public function recordActivity($description)
         {
             $this->activity()->create([
-                'user_id'=>($this->project ?? $this)->owner->id,
+                'user_id'=>auth()->user()->id,
                 'description'=>$description,
                 'changes'=> $this->activityChanges(),
                 'project_id'=>class_basename($this) === 'Project' ? $this->id : $this->project_id
